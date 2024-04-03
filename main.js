e662d92cc703d39b77de587ed86a0d5108dbe2bb
rafaelCOPP <128715716+rafaelCOPP@users.noreply.github.com>
2024-04-03 08:30:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,10 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores= document.querySelectorAll(".contador");
-const tempoObjetivo1= new Date("2024-03-25T00:00:00");
+const tempoObjetivo1= new Date("2029-06-12T00:00:00");
+const tempoObjetivo2= new Date("2025-02-22T00:00:00");
+const tempoObjetivo3= new Date("2027-12-09T00:00:00");
+const tempoObjetivo4= new Date("2028-09-15T00:00:00");
 let tempoAtual= new Date();
 
 contadores[0].textContent= calculaTempo(tempoObjetivo1);
