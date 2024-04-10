d062a149df43ae50e96f91f11cf9d3ef6ca76750
rafaelCOPP <128715716+rafaelCOPP@users.noreply.github.com>
2024-04-10 08:44:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const botoes= document.querySelectorAll(".botao");
 const texto= document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;i <botoes.length;i++){
+  contadores[0].textContent= calculaTempo(tempos[i]);
   botoes[i].onclick=function(){
     
     for(let j=0;j<botoes.length;j++{
@@ -23,7 +24,6 @@ let tempoAtual= new Date();
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-contadores[0].textContent= calculaTempo(tempos[0]);
 
 function calculaTempo(tempoObjetivo){
   let tempoAtual= newDate();
@@ -39,3 +39,16 @@ function calculaTempo(tempoObjetivo){
   return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
   
 }
+
+function atualizaCronometro(){
+  for(let i=0; i<contadores.lenght; i++){
+    contadores[i].textContent=calculaTempo(tempos[i]);
+  }
+}
+
+function comecaCronometro(){
+  atualizaCronometro();
+  setInterval(atualizaCronometro,1000);
+}
+comecaCronometro();
+
