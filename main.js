df4c42c8b9bce2e97a9386c3d888a4ee39d16fc2
rafaelCOPP <128715716+rafaelCOPP@users.noreply.github.com>
2024-04-15 09:10:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const botoes= document.querySelectorAll(".botao");
 const texto= document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;i <botoes.length;i++){
-  contadores[0].textContent= calculaTempo(tempos[i]);
   botoes[i].onclick=function(){
     
     for(let j=0;j<botoes.length;j++{
